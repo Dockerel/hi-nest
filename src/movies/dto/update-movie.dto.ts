246cc9e6055ff31144ed54eb59b5aeef564de525
Dockerel <dgh0001@naver.com>
2023-08-30 21:44:57 +0900

    app.controller.ts added

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,16 +1,3 @@
-// import { IsNumber, IsString } from 'class-validator';
-
-// export class UpdateMovieDto {
-//   @IsString()
-//   readonly title?: string;
-
-//   @IsNumber()
-//   readonly year?: number;
-
-//   @IsString({ each: true })
-//   readonly genres?: string[];
-// }
-
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMovieDto } from './create-movie.dto';
 
